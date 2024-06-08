8ca3e2510d5293db2d3108df73406098d10229cf
Avani-Vijaybhai-Nakrani <avaninakarani92@gmail.com>
2024-06-08 14:50:20 +0530

    Add files via upload

---
+++ throwExample/Main.java
@@ -0,0 +1,13 @@
+package throwExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		throwexample te = new throwexample();
+		te.setMarks(-1);
+		System.out.println(te.getMarks());
+
+	}
+
+}
