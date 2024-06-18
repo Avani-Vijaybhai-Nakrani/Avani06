dd5d9520dbb5d6caa5b89150823b950448fc8cbe
Avani-Vijaybhai-Nakrani <avaninakarani92@gmail.com>
2024-06-18 18:05:58 +0530

    Add files via upload

---
+++ interthreadcommunication/ProducerConsumerExample.java
@@ -0,0 +1,14 @@
+package interthreadcommunication;
+
+public class ProducerConsumerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Q q = new Q();
+		new Producer(q);
+		new Consumer(q);
+
+	}
+
+}
